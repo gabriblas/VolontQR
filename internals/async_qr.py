381b404a0a53ff72173b965428a3be9b7a74c9b6
Gabriele Blasi <gabriele.blasi@proton.me>
2025-07-08 22:17:13 +0200

    Significantly simplified the async code

--- internals/async_qr.py
+++ internals/async_qr.py
@@ -8,47 +8,19 @@ from segno import make as make_qr
 
 from .data_container import QR_ERRORS
 
-QR_ERRORS = list(QR_ERRORS)
-
-Parameters = namedtuple(
-    "Parameters",
-    ["link", "error", "fg_color", "bg_color", "transform", "bytes", "width", "height"],
-)
 Transform = namedtuple("Transform", ["x", "y", "d", "r"])
 
 
-def setup(data, preview):
-    tx = Transform(data.x / 100, data.y / 100, data.d / 100, data.r)
-    params = list()
-    QR_ERRORS = ["l", "m", "q", "h"]
-
-
-    for link in data.valid_links[:1] if preview else data.valid_links:
-        p = Parameters(
-            link,
-            QR_ERRORS[data.err - 1],
-            data.fg_color,
-            data.bg_color,
-            tx,
-            data.bg.bytes,
-            data.bg.width,
-            data.bg.height,
-        )
-        params.append(p)
-
-    return params
-
-
-def make_page(params: Parameters):
-    bg_page = PdfReader(BytesIO(params.bytes)).pages[0]
+async def make_page(link, error, transform, bg_bytes, fg_color, bg_color):
+    bg_page = PdfReader(BytesIO(bg_bytes)).pages[0]
 
     buffer = BytesIO()
-    qr = make_qr(params.link, error=params.error)
-    qr.save(buffer, kind="pdf", dark=params.fg_color, light=params.bg_color)
+    qr = make_qr(link, error=error)
+    qr.save(buffer, kind="pdf", dark=fg_color, light=bg_color)
     stamp = PdfReader(buffer).pages[0]
 
     s = (
-        params.transform.d
+        transform.d
         * min(bg_page.mediabox.width, bg_page.mediabox.height)
         / max(stamp.mediabox.height, stamp.mediabox.width)
     )
@@ -57,20 +29,17 @@ def make_page(params: Parameters):
     xo = stamp.mediabox.width / 2
     yo = stamp.mediabox.height / 2
     stamp.add_transformation(
-        Transformation()
-        .translate(-xo, -yo)
-        .rotate(params.transform.r)
-        .translate(xo, yo),
+        Transformation().translate(-xo, -yo).rotate(transform.r).translate(xo, yo),
         expand=True,
     )
 
-    x = bg_page.mediabox.width * params.transform.x - xo
-    y = bg_page.mediabox.height * params.transform.y - yo
+    x = bg_page.mediabox.width * transform.x - xo
+    y = bg_page.mediabox.height * transform.y - yo
     stamp.add_transformation(Transformation().translate(x, y), expand=True)
 
     new_page = PageObject.create_blank_page(
-        width=params.width, height=params.height
-    )  # todo: can't use bg_page.mediabox?
+        width=bg_page.mediabox.width, height=bg_page.mediabox.height
+    )
     new_page.merge_page(bg_page)
     new_page.merge_page(stamp, expand=True, over=True)
 
@@ -99,22 +68,21 @@ def assemble_pages(pages, optimize=True):
 
 async def make(data, pages, preview, optimize=True):
     # setup the parameters for parallel page generation
-    params = await asyncio.to_thread(setup, data, preview)
+    # params = await asyncio.to_thread(setup, data, preview)
+    tx = Transform(data.x / 100, data.y / 100, data.d / 100, data.r)
+    links = data.valid_links[:1] if preview else data.valid_links
+    error = list(QR_ERRORS)[data.err - 1]
 
-    # parallel page generation
-    loop = asyncio.get_running_loop()
+    tasks = [
+        make_page(link, error, tx, data.bg.bytes, data.fg_color, data.bg_color)
+        for link in links
+    ]
 
-    with ProcessPoolExecutor() as executor:
-        # Submit all tasks
-        tasks = [loop.run_in_executor(executor, make_page, p) for p in params]
-
-        # Process tasks as they complete
-        for future in asyncio.as_completed(tasks):
-            page = await future
-            pages.append(page)
+    for future in asyncio.as_completed(tasks):
+        page = await future
+        pages.append(page)
 
     # assemble pages
-    # pdf_buffer = await asyncio.to_thread(assemble_pages, pages, optimize)
     pdf_buffer = assemble_pages(pages, optimize)
 
     return pdf_buffer
